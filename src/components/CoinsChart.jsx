dae1513965e65597354a1fd64beacbc59e4541d3
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-03-25 17:56:13 +0100

    chore: refactor coins chart component

--- src/components/CoinsChart.jsx
+++ src/components/CoinsChart.jsx
@@ -17,18 +17,17 @@ const CoinsChart = () => {
 
   const { currency, symbol } = useAppContext();
 
-  const fetchCoins = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(CoinList(currency));
-      setCoins(data);
-    } catch (err) {
-      console.log(err);
-    }
-    setLoading(false);
-  };
-
   useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(CoinList(currency));
+        setCoins(data);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
     fetchCoins();
   }, [currency]);
 
@@ -49,23 +48,23 @@ const CoinsChart = () => {
   };
 
   return (
-    <div className="bg-gradient-to-r from-gray-900 to-gray-800  min-h-[35vh]">
-      <p className="text-center text-gray-200 font-semibold text-sm md:text-2xl py-4">
-        Today's Cryptocurrency Prices by Market Cap
-      </p>
+    <div className="rounded-xl bg-gray-700/80 min-h-[35vh] mt-10 py-10 px-4 md:px-8 lg:px-10 border border-slate-900">
+      <div className="w-full flex justify-between items-end pb-6 ">
+        <p className="text-gray-200 font-semibold text-2xl">Cryptocurrencies</p>
+      </div>
 
-      <div className="px-4  md:px-20">
+      <div className="">
         <div className=" w-full md:w-full">
           <input
             type="text"
             placeholder="Search For a Crypto Currency..."
             onChange={(e) => setSearch(e.target.value)}
-            className="w-full rounded-lg px-4 py-2 placeholder-gray-400 bg-transparent border border-gray-700 text-gray-400"
+            className="w-full rounded-lg px-4 py-1.5 placeholder-gray-600 bg-gray-900 border border-gray-700 text-gray-200"
           />
         </div>
-        <div className="overflow-x-scroll">
-          <div className="py-6   w-[170vw]   md:w-full">
-            <div className="flex w-full bg-slate-500 text-gray-200 text-lg italic text-end font-medium py-2  px-10 rounded-t-lg">
+        <div className="overflow-x-auto">
+          <div className="py-6 min-w-[40rem]">
+            <div className="flex w-full bg-gray-900/30 border border-slate-700 text-gray-200 text-lg text-end font-medium py-2 px-4 md:px-6 xl:px-10 rounded-t-lg">
               <div className="basis-1/4 text-start">Name</div>
               <div className="basis-1/4 ">24h change</div>
               <div className="basis-1/4">Price </div>
@@ -85,36 +84,40 @@ const CoinsChart = () => {
                 />{" "}
               </div>
             ) : (
-              <div>
+              <div className=" border-slate-700">
                 {handleSearch()
                   .slice((page - 1) * 10, (page - 1) * 10 + 10)
                   .map((coin, id) => (
                     <div
                       onClick={() => router.push(`/${coin.id}`)}
                       key={id}
-                      className="flex md:w-full text-end md:px-10 py-4 text-gray-200  text-lg cursor-pointer border-b border-gray-600 hover:bg-gray-600"
+                      className="flex md:w-full text-end px-4 md:px-6 xl:px-10 py-4 text-gray-200  text-lg cursor-pointer border-b border-gray-600 hover:bg-gray-900"
                     >
                       <div className="basis-1/4 text-start flex items-center gap-4">
-                        <Image src={coin.image} width="45" height="45" />
+                        <Image
+                          src={coin.image}
+                          width="25"
+                          height="25"
+                          alt="icon"
+                        />
                         <p className="font-semibold">{coin.name}</p>
                       </div>
                       <div
                         className={
                           coin?.price_change_percentage_24h < 0
-                            ? "text-red-600 basis-1/4"
-                            : "text-green-600 basis-1/4"
+                            ? "text-red-600 basis-1/4 text-sm font-bold pr-3"
+                            : "text-green-600 basis-1/4 text-sm font-bold pr-3"
                         }
                       >
                         {coin.price_change_percentage_24h.toFixed(2)}%
                       </div>
-                      <div className="basis-1/4">
+                      <div className="basis-1/4 text-sm">
                         {coin.current_price.toLocaleString("en-US", {
                           minimumFractionDigits: 2,
                           maximumFractionDigits: 2,
-                        })}{" "}
-                        {symbol}
+                        })}
                       </div>
-                      <div className="basis-1/4">
+                      <div className="basis-1/4 text-sm">
                         {coin.market_cap.toLocaleString("en-US")} {symbol}
                       </div>
                     </div>
