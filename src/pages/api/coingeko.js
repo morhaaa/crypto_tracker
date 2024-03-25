52d1d4e9fafbfe1f350a5ea2a8db0ab42f1faa3e
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-03-25 17:55:45 +0100

    feat: add api key coin gecko

--- src/pages/api/coingeko.js
+++ src/pages/api/coingeko.js
@@ -1,11 +1,12 @@
+const key = process.env.NEXT_PUBLIC_COIN_GECKO_API_KEY;
 export const CoinList = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${key}`;
 
 export const SingleCoin = (id) =>
-  `https://api.coingecko.com/api/v3/coins/${id}`;
+  `https://api.coingecko.com/api/v3/coins/${id}&x_cg_demo_api_key=${key}`;
 
 export const HistoricalChart = (id, days = 365, currency) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}&x_cg_demo_api_key=${key}`;
 
 export const Trending = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&x_cg_demo_api_key=${key}`;
