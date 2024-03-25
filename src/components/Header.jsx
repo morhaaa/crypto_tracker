221271317118117b44f7c439f9543da2c6ed70fd
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-03-25 17:53:45 +0100

    chore: refactor header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import { useAppContext } from "context/AppContext";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const currencies = [
   { name: "usd", symbol: "$" },
@@ -10,38 +17,41 @@ const currencies = [
 ];
 
 const Header = () => {
-  const { setCurrency, setSymbol } = useAppContext();
+  const { currency, setCurrency, setSymbol } = useAppContext();
 
   const router = useRouter();
 
   return (
-    <nav className="bg-gradient-to-r from-gray-900 to-gray-800 px-4 py-2 md:min-h-[5vh] flex justify-between items-center drop-shadow-2xl">
+    <nav className="p-3 border-b  bg-gray-700/80 border-slate-800 flex justify-between items-center drop-shadow-2xl">
       <h1
         onClick={() => {
           router.push("/");
         }}
-        className="font-extrabold italic text-transparent md:text-3xl bg-clip-text bg-gradient-to-b from-gray-200 to-gray-400 cursor-pointer"
+        className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-b from-gray-200 to-gray-400 cursor-pointer"
       >
         Crypto Tracker
       </h1>
 
-      <select
-        onChange={(e) => {
-          setCurrency(e.target.value);
+      <Select
+        onValueChange={(e) => {
+          console.log(e);
+          setCurrency(e);
           setSymbol(
-            currencies.map((value) =>
-              value.name == e.target.value ? value.symbol : ""
-            )
+            currencies.map((value) => (value.name == e ? value.symbol : ""))
           );
         }}
-        className="rounded-lg px-2 my-1 bg-gray-300 text-gray-700 font-semibold"
       >
-        {currencies.map((value, id) => (
-          <option key={id} value={value.name}>
-            {value.name.toUpperCase()}
-          </option>
-        ))}
-      </select>
+        <SelectTrigger className="w-[80px] bg-transparent text-white border-none">
+          <SelectValue placeholder={currency.toUpperCase()} />
+        </SelectTrigger>
+        <SelectContent className=" bg-gray-700/90 border-slate-800 text-white">
+          {currencies.map((value, id) => (
+            <SelectItem key={id} value={value.name}>
+              {value.name.toUpperCase()}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </nav>
   );
 };
