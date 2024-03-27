4a22b2b35c8f68f041ec47d2fa85f35aeee8efdd
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-03-27 11:58:22 +0100

    feat: refactor single coin page

--- src/pages/[id].jsx
+++ src/pages/[id].jsx
@@ -3,129 +3,60 @@ import axios from "axios";
 import Head from "next/head";
 import { SingleCoin } from "@/pages/api/coingeko";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import { useAppContext } from "context/AppContext";
-import ReactHtmlParser from "react-html-parser";
-import Chart from "../components/Chart";
 import { Dna } from "react-loader-spinner";
+import CandlestickChart from "@/components/CandlestickChart";
+import { InfoCoin } from "@/components/InfoCoin";
 
 const Coins = () => {
   const id = useRouter().query.id;
-  const { currency, symbol } = useAppContext();
 
   const [coin, setCoin] = useState("");
   const [loading, setLoading] = useState(true);
 
-  const fetchCoin = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(SingleCoin(id));
-      setCoin(data);
-    } catch (err) {
-      console.log(err);
-    }
-    setLoading(false);
-  };
-
   useEffect(() => {
-    id == undefined ? "" : fetchCoin();
+    if (!id) return;
+
+    const fetchCoin = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(SingleCoin(id));
+        setCoin(data);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
+    fetchCoin();
   }, [id]);
 
   return (
-    <>
+    <div className="flex-1 flex">
       <Head>
         <title>Crypto Tracker || {id}</title>
         <meta name="description" content="Crypto Tracker app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div>
-        <div className="bg-gradient-to-r from-gray-900 to-gray-800 flex flex-col lg:flex-row py-4">
-          {loading ? (
-            <div className="flex w-full h-full items-center justify-center">
-              {" "}
-              <Dna
-                visible={true}
-                height="80"
-                width="80"
-                ariaLabel="dna-loading"
-                wrapperStyle={{}}
-                wrapperClass="dna-wrapper"
-              />{" "}
+      <div className="flex-1 flex flex-col items-center justify-center lg:flex-row ">
+        {loading ? (
+          <Dna
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        ) : (
+          <div className="flex-1 min-h-full flex justify-center px-6 pt-6 items-center ">
+            <div className="bg-gradient-to-bl from-gray-700/20 via-gray-700/40 to-gray-700/60 border border-slate-800 rounded-md drop-shadow-md flex flex-col lg:flex-row items-center gap-4 py-2 sm:py-4 md:py-6 px-4 lg:px-8 w-[95vw] min-h-full">
+              <InfoCoin coin={coin} />
+              <CandlestickChart coin={coin} />
             </div>
-          ) : (
-            <>
-              <div className="basis-2/6 border-r px-8 py-6">
-                <div className="flex w-full flex-col gap-3 items-center">
-                  <div className="relative h-[200px] w-[200px]">
-                    <Image
-                      src={coin?.image?.large}
-                      alt={coin.name}
-                      fill
-                      className="object-cover "
-                    />
-                  </div>
-                  <p className="font-bold text-gray-200 text-3xl">
-                    {coin?.name}
-                  </p>{" "}
-                  <p className="text-gray-200 italic text-center">
-                    {ReactHtmlParser(coin?.description?.en.split(". ")[0])}
-                  </p>
-                </div>
-
-                <div className="flex flex-col py-4 lg:py-8 gap-2">
-                  <div className="flex gap-2 justify-between">
-                    <p className="text-gray-200  italic  text-2xl md:text-3xl">
-                      Rank:{" "}
-                    </p>
-                    <p className="text-gray-200  font-bold   text-2xl md:text-3xl">
-                      {coin?.market_cap_rank}
-                    </p>
-                  </div>
-
-                  <div className="flex justify-between">
-                    <p className="text-gray-200  italic  text-2xl md:text-3xl">
-                      Current Price:{" "}
-                    </p>
-                    <p className="text-gray-200  font-bold   text-2xl md:text-3xl">
-                      {coin?.market_data?.current_price[
-                        `${currency}`
-                      ].toLocaleString("en-US", {
-                        maximumFractionDigits: 2,
-                      })}
-                      {symbol}
-                    </p>
-                  </div>
-
-                  <div className="flex justify-between">
-                    <p className="text-gray-200  italic  text-2xl md:text-3xl">
-                      24h Change:{" "}
-                    </p>
-                    <p
-                      className={
-                        coin?.market_data?.price_change_percentage_24h >= 0
-                          ? "text-green-400  font-bold   text-2xl md:text-3xl"
-                          : "text-red-400  font-bold   text-2xl md:text-3xl"
-                      }
-                    >
-                      {coin?.market_data?.price_change_percentage_24h.toLocaleString(
-                        "en-US",
-                        {
-                          maximumFractionDigits: 2,
-                        }
-                      )}
-                      %
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="basis-4/6  px-2 lg:px-8 py-6">
-                <Chart coin={coin} />
-              </div>
-            </>
-          )}
-        </div>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
